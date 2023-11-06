0aa94e76bec9e99d5e6e0f60fd1b043154480ed7
socar-dini <0327jane@gmail.com>
2023-11-06 14:41:34 +0900

    feat: change site logo

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,6 @@
 import siteMetadata from '@/data/siteMetadata'
 import headerNavLinks from '@/data/headerNavLinks'
+import Logo from '@/data/avatar.svg'
 import Link from './Link'
 import MobileNav from './MobileNav'
 import ThemeSwitch from './ThemeSwitch'
@@ -11,6 +12,9 @@ const Header = () => {
       <div>
         <Link href="/" aria-label={siteMetadata.headerTitle}>
           <div className="flex items-center justify-between">
+            <div className="mr-3">
+              <Logo />
+            </div>
             {typeof siteMetadata.headerTitle === 'string' ? (
               <div className="hidden h-6 text-2xl font-semibold sm:block">
                 {siteMetadata.headerTitle}
